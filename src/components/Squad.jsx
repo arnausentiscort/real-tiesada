bcdf4662e6d7f544e478db4a0da551973b1b9f3e
arnausentiscort <srxopet@gmail.com>
2026-03-13 16:54:18 +0100

    Fix Squad.jsx div de mes

--- src/components/Squad.jsx
+++ src/components/Squad.jsx
@@ -144,8 +144,6 @@ export default function Squad() {
         </p>
       </div>
 
-      </div>
-
       {/* Mòbil: carousel horitzontal / Desktop: grid */}
       <div className="hidden sm:grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
         {sorted.map(player => (
